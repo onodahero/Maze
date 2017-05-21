bea216324e816477b2c89180d46fe68000d204aa
斧田洋人 <hiroto@s-hiroto-onoda-no-macbook-pro.local>
2017-05-21 10:40:11 +0900

    Initial Commit

---
+++ maze1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  maze1
+//
+//  Created by 斧田洋人 on 2017/05/21.
+//  Copyright © 2017年 斧田洋人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ maze1Tests/maze1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  maze1Tests.swift
+//  maze1Tests
+//
+//  Created by 斧田洋人 on 2017/05/21.
+//  Copyright © 2017年 斧田洋人. All rights reserved.
+//
+
+import XCTest
+@testable import maze1
+
+class maze1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
